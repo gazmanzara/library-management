0334005efa3e1fe6561fd61e4ce2dd995450b633
Gagaz Manqunazara <gazmanzara@Gagazs-MacBook-Air.local>
2025-05-29 21:32:24 +0700

    fix typo

--- app/lib/axiosInstance.ts
+++ app/lib/axiosInstance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://library-management-api-3cjq.onrender.com/api",
+  baseURL: "https://library-management-api-production-a5d2.up.railway.app/api",
 });
 
 export default axiosInstance;
